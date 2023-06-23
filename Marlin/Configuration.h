63b4f3a79b8edd1927e6f9c73d9e225233c7f28b
will-ks <will@willks.com>
2023-06-23 20:28:48 +0200

    Add name and bootscreen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,9 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "will-ks" // Who made the changes.
+#define SHOW_BOOTSCREEN
+#define SHOW_CUSTOM_BOOTSCREEN
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
